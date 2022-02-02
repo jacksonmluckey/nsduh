2291fc04fd0d3a09cd6776dc6b676ca7a555e68e
Jackson M Luckey <7769009+jacksonmluckey@users.noreply.github.com>
2022-02-01 22:02:43 -0300

    throwing something up since I have GitHub working

---
+++ scrape.py
@@ -0,0 +1,15 @@
+import requests
+
+# generate a list of all years that SAMHSA has a NSDUH survey for in SAMHDA
+years = []
+for year in range(1990, 2021):
+  years.append(year)
+years.append(1988)
+years.append(1985)
+years.append(1979)
+
+# download the delimited zip file for year year
+for year in years:
+  year = str(year)
+  url = "https://www.datafiles.samhsa.gov/sites/default/files/field-uploads-protected/studies/NHSDA-" + year + "/NHSDA-" + year + "-datasets/NHSDA-" + year + "-DS0001/NHSDA-" + year + "-DS0001-bundles-with-study-info/NHSDA-" + year + "-DS0001-bndl-data-tsv.zip"
+  print(url)
