680794256f387c82dbd0cc038e1553e4b0cf574e
Jackson M Luckey <7769009+jacksonmluckey@users.noreply.github.com>
2022-02-02 21:01:11 -0300

    have scraper working. problem seems to be that url changes for some years and delimited is not available for all years. ignoring for now

--- scrape.py
+++ scrape.py
@@ -9,10 +9,18 @@ years.append(1985)
 years.append(1979)
 
 # download the delimited zip file for year year
+print("Downloading the NSDUH datasets from SAMHDA...")
+
 for year in years:
   year = str(year)
+  print("Now downloading " + year)
   url = "https://www.datafiles.samhsa.gov/sites/default/files/field-uploads-protected/studies/NHSDA-" + year + "/NHSDA-" + year + "-datasets/NHSDA-" + year + "-DS0001/NHSDA-" + year + "-DS0001-bundles-with-study-info/NHSDA-" + year + "-DS0001-bndl-data-tsv.zip"
   r = requests.get(url)
-  z = zipfile.ZipFile(io.BytesIO(r.content))
-  z.extractall(year)
-  print(url)
+  try:
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    destination_path = "data" + "/" + year
+    z.extractall(destination_path)
+  except:
+    print("The zip file for the year " + year + " could not be successfully downloaded and extracted :(")
+  finally:
+    print("Downloaded and extracted NSDUH data for " + year)
